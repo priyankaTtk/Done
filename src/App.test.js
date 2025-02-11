0ddc35dfe86b19013b73119c7f762e8d9b631ef4
Priyanka <priyankathukral2004@gmail.com>
2025-02-12 03:03:06 +0530

    test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; // Import for matchers like toBeInTheDocument
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { act } from 'react'; // Use act from react
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Just Another Movie Website header', async () => {
+    await act(async () => {
+        render(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        );
+    });
+
+    const headerElement = screen.getByText(/Just Another Movie Website/i);
+    expect(headerElement).toBeInTheDocument();
 });
